a2a905a60e41de31d247c2282d6671945d32b7e7
Diego Martín <campos.martin.diego@gmail.com>
2025-07-04 17:53:54 +0200

    Add "-- >" to make script exec debuf easy

--- start_containers.sh
+++ start_containers.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# Create required directories
+mkdir certbot nginx superset_home
+
 # Step 1: Copy the HTTP configuration to the nginx folder
-echo "Copying the HTTP configuration to nginx/nginx.conf"
+echo "-- > Copying the HTTP configuration to nginx/nginx.conf"
 cp nginx/nginx.http.conf nginx/nginx.conf
 
 # Step 2: Start nginx only in HTTP mode
@@ -10,7 +13,7 @@ echo "Starting nginx only in HTTP mode"
 docker-compose up -d nginx
 
 # Step 3: Run Certbot to obtain the certificates
-echo "Running Certbot to obtain the certificates"
+echo "-- > Running Certbot to obtain the certificates"
 docker run --rm -it \
   -v "$(pwd)/certbot/www:/var/www/certbot" \
   -v "$(pwd)/certbot/conf:/etc/letsencrypt" \
@@ -19,11 +22,11 @@ docker run --rm -it \
   -d happyrobot-challenge.duckdns.org -d www.happyrobot-challenge.duckdns.org
 
 if [ $? -ne 0 ]; then
-  echo "ERROR: Certbot failed. Exiting."
+  echo "-- > ERROR: Certbot failed. Exiting."
   exit 1
 fi
 # Step 4: "Start Superset dashboard and create admin user"
-echo "Start Superset dashboard and create admin user"
+echo "-- > Start Superset dashboard and create admin user"
 docker-compose up -d superset
 docker exec -it superset superset db upgrade
 docker exec -it superset superset fab create-admin \
@@ -31,13 +34,13 @@ docker exec -it superset superset fab create-admin \
 docker exec -it superset superset init
 
 # Step 5: Replace the configuration with the HTTPS one
-echo "Replacing the configuration with the HTTPS one"
+echo "-- > Replacing the configuration with the HTTPS one"
 cp nginx/nginx.https.conf nginx/nginx.conf
 
 # Step 6: Restart nginx to activate HTTPS
-echo "Restarting nginx to activate HTTPS"
+echo "-- > Restarting nginx to activate HTTPS"
 docker-compose restart nginx
 
 # Step 7: Start the rest of the services
-echo "Starting the rest of the services"
+echo "-- > Starting the rest of the services"
 docker-compose up -d app db
